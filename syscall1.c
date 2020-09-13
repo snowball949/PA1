8b1fdc86ffe51d6709b306709594fca2bfc35a3a
snowball949 <49416665+snowball949@users.noreply.github.com>
2020-09-13 15:13:38 -0600

    Create syscall1.c

---
+++ syscall1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+int main()
+{
+   long int amma = syscall(333);
+   printf("System call sys_hello returned %ld\n", amma);
+   return 0;
+
+}
